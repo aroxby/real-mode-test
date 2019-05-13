5a47230b79acfcb681e45020c806a6ab071fa663
Andy Roxby <aroxby@users.noreply.github.com>
2019-05-13 19:09:47 -0400

    Add some structure to C code

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
-__asm__("\
-    .code16\n\
-    \n\
-    mov $'*', %al\n\
-    mov $0x3F8, %dx\n\
-    out %al, %dx\n\
-    \n\
-    mov $0, %al\n\
-    out %al, $0xf4\n\
-    \n\
-    cli\n\
-    haltloop:\n\
-    hlt\n\
-    jmp haltloop\n\
-");
+/*
+This instruction (while making the code more readable) is not needed if we are careful.
+I was tempted to leave it in for clarity but this projects intention to achieve full parity
+with hand written assembly.  I leave it here for reference.
+asm("jmp main");
+*/
+
+void main() {
+    asm("\
+        mov $'*', %al\n\
+        mov $0x3F8, %dx\n\
+        out %al, %dx\n\
+        \n\
+        mov $0, %al\n\
+        out %al, $0xf4\n\
+        \n\
+        cli\n\
+        haltloop:\n\
+        hlt\n\
+        jmp haltloop\n\
+    ");
+}
