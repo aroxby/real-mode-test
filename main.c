7893f36c578450bf31b6f80c151a03b2330966ce
Andy Roxby <aroxby@users.noreply.github.com>
2019-05-13 20:47:47 -0400

    Greatly structure program

--- main.c
+++ main.c
@@ -5,18 +5,45 @@ with hand written assembly.  I leave it here for reference.
 asm("jmp main");
 */
 
+/*
+This is not strictly needed.
+The GCC will normally choose to inline our simple functions anyway.
+A simple `inline` will prevent the compilier from generating
+full versions of the function (as well as inlining).
+The "always_inline" should cause a warning to be raised if a function
+is ever not inlined.
+*/
+#define INLINE __attribute__((always_inline)) inline
+
+INLINE void hlt();
+INLINE void cli();
+INLINE void out11(unsigned char value, unsigned char port);
+INLINE void out12(unsigned char value, unsigned short port);
+
 void main() {
-    asm("\
-        mov $'*', %al\n\
-        mov $0x3F8, %dx\n\
-        out %al, %dx\n\
-        \n\
-        mov $0, %al\n\
-        out %al, $0xf4\n\
-        \n\
-        cli\n\
-        haltloop:\n\
-        hlt\n\
-        jmp haltloop\n\
-    ");
+    out12('*', 0x3F8);
+    out11(0, 0xF4);
+    cli();
+    while(1) {
+        hlt();
+    }
+}
+
+INLINE void cli() {
+    asm("cli");
+}
+
+INLINE void hlt() {
+    asm("hlt");
+}
+
+INLINE void out11(unsigned char value, unsigned char port) {
+    asm("mov %0, %%al":: "i"(value));
+    asm("out %%al, %0": : "i"(port));
+}
+
+INLINE void out12(unsigned char value, unsigned short port) {
+    asm("mov %0, %%al": : "i"(value));
+    asm("mov %0, %%dx": : "i"(port));
+    asm("out %al, %dx");
 }
